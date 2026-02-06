5e7321a9c97419ff6f1e579ff7ab277bf77d1947
Estudoweb <webdeveloperzin@gmail.com>
2026-02-05 22:43:48 -0300

    Anti-inspect

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
-<script src="anti-inspect.js"></script>
+import "./anti-inspect.js";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
